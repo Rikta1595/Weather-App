ca274da682fb9e38ce95546bcef4624ee3287865
Manisha Mukherjee <manisha.rikhta95@gmail.com>
2023-02-28 23:03:40 +0530

    modify

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '63734313d2mshce371fe87b00ddfp138c4ajsna55a1c58eef2',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,5 +10,5 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
 
-export const WEATHER_API_KEY = "e8ce4161050027a29ba74e977d7bee71";
+export const WEATHER_API_KEY = "";
 
